c605a473a3a49f602af566286b34b4d1a553dd07
christian españa chamorro <christian.espana1@gmail.com>
2019-09-20 04:50:10 -0500

    first commit

---
+++ glob.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 19:59:46 2019
+
+@author: CDEC
+"""
+
+import glob
+
+
+chars = glob.glob('D:\Documents\OPENCV\TRAINING')
+
+from pathlib import Path
+
+'''for filename in glob.glob('*'):
+    print(filename)
+    
+print('-----------------------')    
+
+for filename in Path('D:\Documents\OPENCV\TRAINING').glob('*'):
+    print(filename)
+
+
+print('-----------------------') '''
+
+
+for caracter in glob.glob('D:\Documents\OPENCV\TRAINING' +'\*'):
+    print('\n')
+    print(caracter)
+ 
+    for x in Path(caracter).glob('*.jpg'):
+        print(x)
+       
